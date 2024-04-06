f7d2c0b50fa6f10bbe69c21f09aaa6cd840ff425
Shahriar Shopno <shariare.shwapno@gmail.com>
2024-04-06 18:37:45 +0600

    set up 2 user buttons

--- main.py
+++ main.py
@@ -23,6 +23,26 @@ class Face_Rec_Sys:
         bg_lbl=Label(self.root,image=self.photoimg2)
         bg_lbl.place(x=0,y=200,width=1300,height=700)
 
+#user_buttons
+        img3=Image.open(r"D:\Work X Git\OOP 2 Project\Images\attendance.png")
+        img3=img3.resize((200,200))
+        self.photoimg3=ImageTk.PhotoImage(img3)
+
+        b1=Button(bg_lbl,image=self.photoimg3,cursor="hand2")
+        b1.place(x=100,y=200,width=200,height=200)
+        b1_t=Button(bg_lbl,text="Attendance",cursor="hand2",font=("arial",20,"bold"),bg="skyblue",fg="darkblue")
+        b1_t.place(x=100,y=400,width=200,height=30)
+
+    #button 2 - Face detector
+
+        img4=Image.open(r"D:\Work X Git\OOP 2 Project\Images\face.png")
+        img4=img4.resize((200,200))
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b2=Button(bg_lbl,image=self.photoimg4,cursor="hand2")
+        b2.place(x=400,y=200,width=200,height=200)
+        b2_t=Button(bg_lbl,text="Face Detector",cursor="hand2",font=("arial",20,"bold"),bg="skyblue",fg="darkblue")
+        b2_t.place(x=400,y=400,width=200,height=30)
 
 if __name__ == "__main__":
     root=Tk()
