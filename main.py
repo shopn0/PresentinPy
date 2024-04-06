d0f33dbd47e77cad6abfb8e21565d3be90560d5e
Shahriar Shopno <shariare.shwapno@gmail.com>
2024-04-06 15:31:22 +0600

    bg image set

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ class Face_Rec_Sys:
         self.root=root
         self.root.geometry("1300x900")
         self.root.title("PresentinPy")
-
+        self.root.resizable(False,False)
+#Heading banner x logo / intro
         img=Image.open(r"D:\Work X Git\OOP 2 Project\Images\banner.png")
         img=img.resize((1300,200))
         self.photoimg=ImageTk.PhotoImage(img)
 
         f_lbl=Label(self.root,image=self.photoimg)
         f_lbl.place(x=0,y=0,width=1300,height=200)
+#bg image
+        img2=Image.open(r"D:\Work X Git\OOP 2 Project\Images\bg_home.png")
+        img2=img2.resize((1300,700))
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+        bg_lbl=Label(self.root,image=self.photoimg2)
+        bg_lbl.place(x=0,y=200,width=1300,height=700)
 
 
 if __name__ == "__main__":
